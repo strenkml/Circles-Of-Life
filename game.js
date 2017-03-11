58973b482c6f2c4ff87643236ab2e7abc4971bbc
fardenml <mfarden@gmail.com>
2017-03-11 01:15:44 -0500

    Create Food

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var theta, canvas, gl;
 var myShaderProgramCircle, myShaderProgramFood;
-var n = 100;
+var bufferIdCircle, bufferIdFood;
+var arrayOfPointsForCircle, arrayOfPointsForFood;
 var score = 0;
 
 //Animation global variables
@@ -8,6 +9,7 @@ var coordinatesUniform;
 var stepX, stepY;
 var directionX, directionY;
 var stepScale;
+var thetaUniform;
 
 function init() {
 	canvas = document.getElementById("gl-canvas");
@@ -17,14 +19,12 @@ function init() {
 	gl.clearColor(0, 0, 0, 1);
 
 	myShaderProgramCircle = initShaders(gl,"vertex-shader", "fragment-shader-circle");
-	gl.useProgram(myShaderProgramCircle);
 	drawCircle();
 
-	//myShaderProgramFood = initShaders(gl,"vertex-shader", "fragment-shader-food");
-	//gl.useProgram(myShaderProgramFood);
-
+	myShaderProgramFood = initShaders(gl,"vertex-shader2", "fragment-shader-food"); 
+	drawFood();
+	
 	theta = .0;
-	rotateFlag = 0;
 	stepX = .0; stepY = .0;
 	directionX = .0; directionY = .0;
 	stepScale = .01;
@@ -34,36 +34,79 @@ function init() {
 
 	document.getElementById('score').innerHTML = score;
 
-
 	render();
 }
 
-function drawCircle() {
-	var arrayOfPointsForCircle = [];
-
-	var x,y;
-
-	var thetaStart = 0;
-	var thetaEnd = 2 * Math.PI;
-	var thetaStep = (thetaEnd - thetaStart)/n;
-
-	var myPoint;
-
-	for (var i = 0; i < n; i++) {
-		theta = thetaStart + i * thetaStep;
-		x = Math.cos(theta)/10;
-		y = Math.sin(theta)/10;
-		myPoint = vec2(x,y);
-		arrayOfPointsForCircle.push(myPoint);
-	}
-
-	var bufferId = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(arrayOfPointsForCircle), gl.STATIC_DRAW);
-
+function render() {
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	
+	// Circle shader program
+	gl.useProgram(myShaderProgramCircle);
+	
+	thetaUniform = gl.getUniformLocation(myShaderProgramCircle, "theta");
+	gl.uniform1f(thetaUniform, theta);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdCircle);
+	
 	var myPosition = gl.getAttribLocation(myShaderProgramCircle, "myPosition");
 	gl.vertexAttribPointer(myPosition, 2, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(myPosition);
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, 100);
+	
+	gl.uniform2f(coordinatesUniform, stepX, stepY);
+	stepX += directionX;
+	stepY += directionY;
+	
+	// Food shader program
+	gl.useProgram(myShaderProgramFood);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdFood);
+	
+	var myPositionFood = gl.getAttribLocation(myShaderProgramFood, "foodPosition");
+	gl.vertexAttribPointer(myPositionFood, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(myPositionFood);
+	
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, 100);
+	
+	requestAnimFrame(render);
+}
+
+function drawCircle() {
+	arrayOfPointsForCircle = [];
+	var thetaStart = 0;
+	var thetaEnd = 2 * Math.PI;
+	var thetaStep = (thetaEnd - thetaStart)/100;
+
+	for (var i = 0; i < 100; i++) {
+		theta = thetaStart + i * thetaStep;
+		var x = Math.cos(theta)/10;
+		var y = Math.sin(theta)/10;
+		var myPoint = vec2(x,y);
+		arrayOfPointsForCircle.push(myPoint);
+	}
+
+	bufferIdCircle = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdCircle);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(arrayOfPointsForCircle), gl.STATIC_DRAW);
+}
+
+function drawFood() {
+	arrayOfPointsForFood = [];
+	var thetaStart = 0;
+	var thetaEnd = 2 * Math.PI;
+	var thetaStep = (thetaEnd - thetaStart)/100;
+
+	for (var i = 0; i < 100; i++) {
+		theta = thetaStart + i * thetaStep;
+		var x = Math.cos(theta)/30;
+		var y = Math.sin(theta)/30;
+		var myPoint = vec2(x,y);
+		arrayOfPointsForFood.push(myPoint);
+	}
+
+	bufferIdFood = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdFood);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(arrayOfPointsForFood), gl.STATIC_DRAW);
 }
 
 function moveCircle(event) {
@@ -95,51 +138,7 @@ function moveCircleKeys(event) {
 	}
 }
 
-function render() {
-	var thetaUniform = gl.getUniformLocation(myShaderProgramCircle, "theta");
-
-	gl.uniform1f(thetaUniform, theta);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
-	gl.uniform2f(coordinatesUniform, stepX, stepY);
-	stepX += directionX;
-	stepY += directionY;
-	requestAnimFrame(render);
-}
-
 function updateScore() {
 	score += 10;
 	document.getElementById('score').innerHTML = score;
-}
-
-function drawFood(){
-	var arrayOfPointsForFood = [];
-
-	var x,y;
-
-	var thetaStart = 0;
-	var thetaEnd = 2 * Math.PI;
-	var thetaStep = (thetaEnd - thetaStart)/n;
-
-	var myPoint;
-
-	for (var i = 0; i < n; i++) {
-		theta = thetaStart + i * thetaStep;
-		x = Math.cos(theta)/10;
-		y = Math.sin(theta)/10;
-		myPoint = vec2(x,y);
-		arrayOfPointsForFood.push(myPoint);
-	}
-
-	var bufferId = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(arrayOfPointsForFood), gl.STATIC_DRAW);
-
-	var myPosition = gl.getAttribLocation(myShaderProgramFood, "myPosition");
-	gl.vertexAttribPointer(myPosition, 2, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(myPosition);
-}
-
-function drawEnemy(){
-
 }
